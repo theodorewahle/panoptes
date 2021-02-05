366b01dc10c29d47477adab700e0c10e7b196710
JamesTann <jt.21@dartmouth.edu>
2021-02-05 12:35:53 -0500

    Added basic OpenCV hog detection model

---
+++ computer-vision/hog_detection.py
@@ -0,0 +1,53 @@
+import cv2 as cv
+import numpy as np
+
+class HOGDetectionModel():
+
+    def __init__(self):
+        
+        # initialize the HOG descriptor
+        self.hog = cv.HOGDescriptor()
+
+        # use prebuilt person detector
+        self.hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
+
+    def detect(self, filepath):
+
+        # open video
+        video = cv.VideoCapture(filepath)
+
+        while True:
+
+            ret, frame = video.read()
+
+            if frame is None:
+                break
+
+            for (xA, yA, xB, yB) in self.find_boxes(frame):
+
+                # display the detected boxes in the colour picture
+                cv.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+            yield frame
+
+    # helper function for placing boxes around people
+    def find_boxes(self, frame):
+    
+        # convert to grayscale
+        gray = cv.cvtColor(frame, cv.COLOR_RGB2GRAY)
+
+        # detect people in the image
+        # returns the bounding boxes for the detected objects
+        boxes, weights = self.hog.detectMultiScale(frame, winStride=(8,8) )
+
+        # convert to numpy array to draw the boxes
+        return np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+
+
+hog_detect = HOGDetectionModel()
+
+for frame in hog_detect.detect('kramer_walking.mp4'):
+    cv.imshow("frame", frame)
+    cv.waitKey(30)
+
+cv.destroyAllWindows()
