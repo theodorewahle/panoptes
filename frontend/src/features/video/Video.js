d0af8ab5b50c073066764f38c2dde2857466c8eb
Jack McGraw <jckmcgraw@me.com>
2021-02-09 15:55:50 -0800

    fixed Video width/height prop

--- frontend/src/features/video/Video.js
+++ frontend/src/features/video/Video.js
@@ -4,7 +4,6 @@ import styles from './Video.module.scss';
 
 const Video = (props) => {
   const { width, height, title, url } = props;
-  let videoWidth, videoHeight;
   // TODO: have prop be option of size in text then convert
   //       to dimensions here
   if (width == null || height == null || url == null) {
@@ -17,7 +16,7 @@ const Video = (props) => {
   }
   return (
     <div className={styles.videoPlayer}>
-      <ReactPlayer url={url} width={videoWidth} height={videoHeight} />
+      <ReactPlayer url={url} width={width} height={height} />
       <div className={styles.title}>{displayTitle}</div>
     </div>
   );
