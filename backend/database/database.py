30a693d5444649c71097a0c1684cf421bd2d4f11
JamesTann <jt.21@dartmouth.edu>
2021-02-16 10:00:19 -0500

    Commenting, set up try for schema creation

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,13 +1,30 @@
 from models import Camera, Base
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.schema import CreateSchema
+from sqlalchemy import create_engine
+
+# shema name, should be moved to config eventually
+SCHEMA_NAME = 'panoptes'
 
 class DatabaseHelper():
 
+
     def __init__(self, app):
+
+        # attempt to create schema if it doesn't exist
+        engine = create_engine('mysql://root:password@localhost')
+        try:
+            engine.execute(CreateSchema(SCHEMA_NAME))
+        except:
+            print(SCHEMA_NAME + " already exists.")
+
+        # set up flask app config options, will be moved to config file eventually
         self.app = app;
-        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/panoptes'
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/' + SCHEMA_NAME
         self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        
+        # set up database and create tables
         self.db = SQLAlchemy(self.app, model_class=Base)
         self.db.create_all()
 
@@ -24,6 +41,7 @@ class DatabaseHelper():
 
         
 database_helper = DatabaseHelper(Flask(__name__))
+database_helper.add_camera('www.some.url.com')
 print(database_helper.list_tables())
 for camera in database_helper.get_all_cameras():
     print(str(camera.camera_id) + " " + camera.url)
