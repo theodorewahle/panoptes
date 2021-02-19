076587fb8f45f0f0d2396f1bbcbefaee80de1bea
JamesTann <jt.21@dartmouth.edu>
2021-02-19 16:35:04 -0500

    Basic db helpers

--- backend/database/database.py
+++ backend/database/database.py
@@ -27,14 +27,23 @@ class DatabaseHelper():
         self.db = SQLAlchemy(app, model_class=models.Base)
         self.db.create_all()
 
+    def get_all_cameras(self):
+        return self.db.session.query(models.Camera).all()
+
     def add_camera(self, url):
         camera = models.Camera(url=url)
         self.db.session.add(camera)
         self.db.session.commit()
         return camera
 
-    def get_all_cameras(self):
-        return self.db.session.query(models.Camera).all()
+    def get_all_videos(self):
+        return self.db.session.query(models.Video).all()
+
+    def add_video(self, file_path, camera_id):
+        video = models.Video(file_path=file_path, camera_id=camera_id)
+        self.db.session.add(video)
+        self.db.session.commit()
+        return video
 
     def get_all_incidents(self):
         return self.db.session.query(models.Incident).all()
@@ -45,3 +54,27 @@ class DatabaseHelper():
                 .join(models.Camera) \
                     .filter(models.Camera.camera_id == camera_id) \
                         .all()
+
+    def add_incident(self, start_time, end_time, object_id, video_id):
+        incident = models.Video(start_time=start_time, end_time=end_time, object_id=object_id, video_id=video_id)
+        self.db.session.add(incident)
+        self.db.session.commit()
+        return incident
+
+    def get_all_objects(self):
+        return self.db.session.query(models.Object).all()
+
+    def add_object(self, name, object_set_id):
+        object = models.Object(name=name, object_set_id=object_set_id)
+        self.db.session.add(object)
+        self.db.session.commit()
+        return object
+
+    def get_all_object_sets(self):
+        return self.db.session.query(models.ObjectSet).all()
+
+    def add_object_set(self, name):
+        object_set = models.ObjectSet(name=name)
+        self.db.session.add(object_set)
+        self.db.session.commit()
+        return object_set
