a11fe1c92dde3b5e18493a1f2e6299047148a502
Teddy <theodorewahle@gmail.com>
2021-02-19 12:51:34 -0500

    Live stream page styling

--- frontend/src/features/pageContainer/pages/LiveStreamPage.js
+++ frontend/src/features/pageContainer/pages/LiveStreamPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectRecentIncidents } from '../../video/videoSlice';
 import VideoThumbnails from '../../video/VideoThumbnails';
-import styles from './LandingPage.module.scss';
+import styles from './LiveStreamPage.module.scss';
 import ENV from '../../../env';
 
 const LiveStreamPage = () => {
@@ -10,6 +10,11 @@ const LiveStreamPage = () => {
 
   return (
     <div>
+      <div> 
+        <video width="720" height="405" controls>
+          <source src="http://localhost:8080/webcam.ogg" type="video/ogg"></source>
+        </video>
+      </div>
       <div className={styles.containerRecentIncidents}>
         <div className={styles.titleRecentIncidents}>Recent Incidents</div>
         <VideoThumbnails
