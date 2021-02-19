187a74b0a1560aeb8e44fbc57366db4634e5da23
Teddy <theodorewahle@gmail.com>
2021-02-18 22:47:24 -0500

    added FTP functionality for download all the day's incidents

--- backend/server.py
+++ backend/server.py
@@ -3,6 +3,7 @@ from flask import Flask, Response, render_template
 from streaming.live_streaming import generate
 from streaming.static import generate_static
 from computer_vision.hog_detection import HOGDetectionModel
+from incidents.ftp import fetch_todays_incidents
 
 # DO NOT CHANGE THIS NAME
 # IT MUST BE NAMED "application" IN ORDER TO BE 
@@ -31,8 +32,11 @@ def ping():
     return "pong"
 
 if __name__ == '__main__':
-   host = "127.0.0.1"
-   port = 8000
-   debug = False
-   options = None
-   application.run(host, port, debug, options)
+    # Fetch the latest incidents from the camera's FTP server
+    fetch_todays_incidents()
+    
+    host = "127.0.0.1"
+    port = 8000
+    debug = False
+    options = None
+    application.run(host, port, debug, options)
