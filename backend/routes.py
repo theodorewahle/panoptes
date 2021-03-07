4de238bcb958abb771e536d3406fd9b311db4ed3
JamesTann <jt.21@dartmouth.edu>
2021-03-07 17:13:06 -0500

    Added thumbnails to database, routes

--- backend/routes.py
+++ backend/routes.py
@@ -4,7 +4,6 @@ routes.py
 Blueprint for routes for backend, actualized by flask server in server.py
 """
 from flask import Blueprint
-from flask.globals import request
 from utils.utils import *
 from flask import Response, render_template, send_file
 from computer_vision.hog_detection import HOGDetectionModel
@@ -43,6 +42,16 @@ def ping():
 @routes.route('/video/<video_id>')
 def send_static_file(video_id):
     result = db_helper.get_video(video_id=video_id)
+
+    if len(result) != 1:
+        abort(404)
+    else:
+        return send_file(result[0].file_path)
+
+
+@routes.route('/incident/<incident_id>')
+def send_thumbnail(incident_id):
+    result = db_helper.get_incident(incident_id=incident_id)
 
     if len(result) != 1:
         abort(404)
