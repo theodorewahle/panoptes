0070d3954aadee9d80f36419ed91c5c0fc873598
Charles Chen <charles.chen.21@dartmouth.edu>
2021-04-02 14:23:48 -0400

    basic auth functions added for jwt

--- backend/database/models.py
+++ backend/database/models.py
@@ -2,6 +2,9 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.sqltypes import DateTime, CHAR
+from backend.server import application
+import jwt
+import datetime
 
 # this file represents all the tables in the database and is used by SQL Alchemy
 
@@ -111,6 +114,40 @@ class User(Base):
     username = Column(String(100), nullable=False, unique=True)
     salt = Column(CHAR(128), nullable=False)
     hash = Column(CHAR(128), nullable=False)
+
+    def encode_auth_token(self, user_id):
+        """
+        Generates the Auth Token
+        :return: string
+        """
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, seconds=5),
+                'iat': datetime.datetime.utcnow(),
+                'sub': user_id
+            }
+            return jwt.encode(
+                payload,
+                application.config.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+
+    @staticmethod
+    def decode_auth_token(auth_token):
+        """
+        Decodes the auth token
+        :param auth_token:
+        :return: integer|string
+        """
+        try:
+            payload = jwt.decode(auth_token, application.config.get('SECRET_KEY'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
 
     def serialize(self):
         return {'user_id': self.user_id,
--- backend/localconfig-example.py
+++ backend/localconfig-example.py
@@ -3,9 +3,11 @@ localconfig-example.py
 
 Example for localconfig.py, file name must be localconfig.py and located in same directory as config.py
 """
+import os
 
 LOCAL_SQL_URL = 'mysql://username:password@localhost'
 TOKENS = {
     "secret-token-1": "example1",
     "secret-token-2": "example2"
 }
+SECRET_KEY = os.urandom(24)
