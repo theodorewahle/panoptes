0429d1533cda4aa08ac95c97a7d99062c37dac2b
Teddy <theodorewahle@gmail.com>
2021-04-20 22:33:58 -0400

    added a Dockerfile and removed some incidents

--- backend/server.py
+++ backend/server.py
@@ -23,7 +23,7 @@ class FlaskThread(Thread):
 
 
 # PanoptesFlaskApp
-# Custom Flask application
+# Custom Flask app
 class PanoptesFlaskApp(Flask):
     def run(self, host=None, port=None, debug=None, load_dotenv=True, **options):
         with self.app_context():
@@ -37,22 +37,23 @@ class PanoptesFlaskApp(Flask):
             self.register_blueprint(routes)
 
             # Fetch the latest incidents from the camera's FTP server
-            FlaskThread(target=fetch_todays_incidents, args=(db_helper,)).start()
-            FlaskThread(target=streamer.launch_proxy_stream).start()
+            # FlaskThread(target=fetch_todays_incidents, args=(db_helper,)).start()
+            # FlaskThread(target=streamer.launch_proxy_stream).start()
 
         super(PanoptesFlaskApp, self).run(host=host, port=port,
                                           debug=debug, load_dotenv=load_dotenv, **options)
 
 
 # DO NOT CHANGE THIS NAME
-# IT MUST BE NAMED "application" IN ORDER TO BE
+# IT MUST BE NAMED "app" IN ORDER TO BE
 # DETECTED BY AWS ELASTIC BEANSTALK
-application = PanoptesFlaskApp(__name__, static_url_path='')
-cors = CORS(application)
+app = PanoptesFlaskApp(__name__, static_url_path='')
+cors = CORS(app)
+
+@app.route('/')
+def main():
+    return 'Hello, World!'
+
 
 if __name__ == '__main__':
-    host = "127.0.0.1"
-    port = 8000
-    debug = False
-    options = None
-    application.run(host, port, debug, options)
+    app.run(host='0.0.0.0', debug=True)
